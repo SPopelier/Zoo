133b3a2ff9b309fec6638c62629f7910f2cc64de
Sarah Popelier <sarah.popelier@hotmail.fr>
2025-06-10 15:11:37 +0200

    création de la classe Animal

--- app.php
+++ app.php
@@ -1,2 +1,15 @@
 <?php
 require __DIR__ . '/vender/autoload.php';
+
+abstract class Animal {
+
+    private string $name;
+
+    public function getName() : string {
+        return $this->name;
+    }
+
+    public function __construct(string $name) {
+        $this->name = $name;
+    }
+}
